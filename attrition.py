54fea7d837d62f97a85b2c5445c94e52fa49ff83
Adura Kinoshi <51264953+pkinoshi@users.noreply.github.com>
2024-08-22 10:46:09 +0100

    Update attrition.py

--- attrition.py
+++ attrition.py
@@ -7,8 +7,6 @@ Created on Wed Aug 21 09:05:27 2024
 
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pickle
 import streamlit as st
